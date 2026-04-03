05458075d95e9e94151a2adee9732be06bf15cf3
moriryo_o <86156534+ryo-morimoto@users.noreply.github.com>
2026-04-03 10:46:45 +0900

    refactor(nvim): tune editor defaults

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Apply local option overrides after LazyVim defaults are loaded.
+require("config.options")
--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,2 +1,5 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Local option overrides loaded from init.lua after LazyVim defaults.
+-- LazyVim defaults reference: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+-- Keep markdown spell checking useful without flagging Japanese text.
+vim.opt.spelllang = { "en", "cjk" }
