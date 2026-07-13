e21d656a464e4621e205523a69d33e84a42fc8e5
moriryo_o <86156534+ryo-morimoto@users.noreply.github.com>
2026-07-13 13:09:33 +0900

    fix: keep nixos build passing

--- nix-config/hosts/ryobox/default.nix
+++ nix-config/hosts/ryobox/default.nix
@@ -124,7 +124,7 @@ in
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-        hash = "sha256-wHW0l15aLswe7gV9WioXo//abd0sJI82I7zIroRG3uU=";
+        hash = "sha256-7g8zDx5RhbptXFyEPtexxkHX8hw/gF001bZ7wX4Mjhs=";
       };
       virtualHosts = {
         "plane.ryobox.xyz" = {
